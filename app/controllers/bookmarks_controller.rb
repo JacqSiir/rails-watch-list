5a8d892371f8af4e4f7664175007cadb47961ff1
Anton Gordon <anton.gordon85@outlook.com>
2022-01-22 15:58:17 +0000

    list view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,13 @@
 class BookmarksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,16 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def create
+  end
+
+  def new
+
+  end
+
+  def show
+  end
+
 end

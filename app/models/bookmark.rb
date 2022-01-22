5edf24f4ba13e60d12b23ff05fbca8c00ad1cda6
Anton Gordon <anton.gordon85@outlook.com>
2022-01-22 14:36:33 +0000

    completed models, starting controllers

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, presence: true
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :list_id, presence: true
+  validates_uniqueness_of :list_id, :scope => [:movie_id]
 end

ff5cec0df500049bd73e935a9091655599d5eb04
Anton Gordon <anton.gordon85@outlook.com>
2022-01-23 19:23:31 +0000

    new lists, display lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :lists, only: [:index, :show, :update, :create]
+  resources :lists, only: [:index, :show, :update, :create, :new]
   resources :bookmarks, only: [:index, :create, :new, :destroy]
 end
